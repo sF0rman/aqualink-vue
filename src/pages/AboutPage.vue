f4de1a5082e424f02b6c756cb46c714d7f8782c9
sf0rman <sebastian@forman.no>
2021-09-10 16:59:16 +0200

    About page gets data from API with language selection

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,13 +1,48 @@
 <template>
-  <div class="page">About</div>
+  <div class="page">
+    <div class="wrapper">
+      <h1>{{ norsk ? "Om " : "About " }}Aqualink</h1>
+      <p v-html="render(text)"></p>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import api from "@/api";
+import marked from "marked";
 export default {
-
-}
+  data() {
+    return {
+      text: "",
+    };
+  },
+  inject: ["norsk"],
+  watch: {
+    norsk() {
+      this.getData();
+    },
+  },
+  methods: {
+    render(x) {
+      return marked(x);
+    },
+    getData() {
+      axios
+        .get(`${api.url}/about`, {
+          params: {
+            _locale: localStorage.getItem("language") ?? "en",
+          },
+        })
+        .then((res) => (this.text = res.data.text))
+        .catch(api.handleError());
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+};
 </script>
 
 <style>
-
 </style>
